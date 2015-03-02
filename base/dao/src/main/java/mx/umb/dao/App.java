89d2a6c01d2e5dd99478bad8576aa234af4da429
@el_uriel <urielux@yahoo.com>
2015-03-02 16:40:51 -0600

    Primer commit.

---
+++ base/dao/src/main/java/mx/umb/dao/App.java
@@ -0,0 +1,13 @@
+package mx.umb.dao;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ base/src/main/java/mx/umb/base/App.java
@@ -0,0 +1,13 @@
+package mx.umb.base;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
